bf479cc54c3f44bd2c3ae9cab0425abeb3b9bac4
Mikel Ugarte <mikelugarterz@hotmail.com>
2024-04-17 00:06:35 -0600

    Add package Angular Material

--- todo-components/src/lib/molecules/menu-option/menu-option.component.ts
+++ todo-components/src/lib/molecules/menu-option/menu-option.component.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { TextComponent } from '../../atoms/text/text.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'molecules-menu-option',
     standalone: true,
     imports: [
-        CommonModule, TextComponent
+        CommonModule, TextComponent, MatIconModule
     ],
-    template: `<div>
+    template: `
+    <div>
+        <mat-icon aria-hidden="false" aria-label="Example home icon" fontIcon="home"></mat-icon>
         <atoms-text>{{textOption}}</atoms-text>
     </div>`,
     styleUrl: './menu-option.component.css',
